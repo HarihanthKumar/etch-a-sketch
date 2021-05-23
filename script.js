9c784f695abada81ffa8fdef61495b2026154a4a
HarihanthKumar <harihanth1011@gmail.com>
2021-05-23 21:45:04 +0530

    Styled the page and also added the eraser, reset and rainbow color functionalities

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
 const boardContainer = document.querySelector('.board-container');
-const boardProperties = boardContainer.getBoundingClientRect();
-const boardWidth = boardProperties.width;
-const boardHeight = boardProperties.height;
-let numOfSquares = 150;
+const sizePicker = document.querySelector('#size-picker');
+const resetButton = document.querySelector('.reset');
+const rainbowButton = document.querySelector('.rainbow');
+const colorPicker = document.querySelector('#color-picker');
+const eraserButton = document.querySelector('.eraser');
+let innerDivs;
 
-for(let i=0;i<numOfSquares;i++){
-    const divsContainer = document.createElement('div');
-    divsContainer.style.width = `${boardWidth}px`;
-    divsContainer.style.display = 'flex';
-    for(let j=0;j<numOfSquares; j++){
-        const innerDiv = document.createElement('div');
-        innerDiv.style.width = (`${(boardWidth / numOfSquares)}px`);
-        innerDiv.style.height = (`${(boardWidth / numOfSquares)}px`);
-        innerDiv.classList.add('inner-div');
-        divsContainer.appendChild(innerDiv);
-    }
-    boardContainer.appendChild(divsContainer);
+function resetBoard(){
+    innerDivs.forEach(divs => {
+        divs.style.backgroundColor = 'white';
+    })
 }
 
-const colorPicker = document.querySelector('.color-picker');
+function normalColorPicker(){
+    innerDivs.forEach(divs => divs.addEventListener('mouseenter', function(e){
+        e.target.style.backgroundColor = document.querySelector('#color-picker').value;
+    }));
+}
+function createBoard(){
+    const boardProperties = boardContainer.getBoundingClientRect();
+    let boardWidth = boardProperties.width;
+    let numOfSquares = (101-sizePicker.value);
+    let eachSquareWidth = (boardWidth / numOfSquares);
+    boardContainer.innerHTML = '';
+    for(let i=0;i<numOfSquares;i++){
+        let innerDivsContainer = document.createElement('div');
+        innerDivsContainer.style.display = 'flex';
+        for(let j=0; j<numOfSquares; j++){
+            let innerDiv = document.createElement('div');
+            innerDiv.style.width = `${eachSquareWidth}px`;
+            innerDiv.style.height = `${eachSquareWidth}px`;
+            innerDiv.classList.add('inner-div');
+            innerDivsContainer.appendChild(innerDiv);
+        }
+        boardContainer.appendChild(innerDivsContainer);
+        innerDivs = document.querySelectorAll('.inner-div');
+        normalColorPicker();
+    }
+}
 
-const innerDivs = document.querySelectorAll('.inner-div');
-innerDivs.forEach(div => div.addEventListener('mouseenter', function(e){
-    e.target.style.backgroundColor = colorPicker.value;
-}));
+function customColorGrid(){
+    innerDivs.forEach(divs => divs.addEventListener('mouseenter', function(e){
+        let redValue = Math.round(Math.random() * 255);
+        let blueValue = Math.round(Math.random() * 255);
+        let greenValue = Math.round(Math.random() * 255);
+        e.target.style.backgroundColor = `rgb(${redValue}, ${blueValue}, ${greenValue})`;
+    }));
+}
+
+function eraseBackground(){
+    innerDivs.forEach(divs => divs.addEventListener('mouseenter', function(e){
+        e.target.style.backgroundColor = 'white';
+    }));
+}
+createBoard();
+resetButton.addEventListener('click', resetBoard);
+sizePicker.addEventListener('input', createBoard);
+rainbowButton.addEventListener('click', customColorGrid);
+colorPicker.addEventListener('input', normalColorPicker);
+colorPicker.addEventListener('click', normalColorPicker);
+eraserButton.addEventListener('click', eraseBackground);
